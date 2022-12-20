db56dccc20776903e4a8eaba5fd67f26ec6f9e7e
Ellina Korzhova <ellina.korzhpva@mail.ru>
2022-12-20 20:54:23 +0300

    Домашнее задание 8

---
+++ Example2/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+
+int[,] InitArray(int i, int j)
+{
+    int[,] array = new int[i, j];
+    Random rnd = new Random();
+    for (i = 0; i < array.GetLength(0); i++)
+    {
+        for (j = 0; j < array.GetLength(1); j++)
+        {
+
+            array[i, j] = rnd.Next(0, 10);
+
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] array = InitArray(4, 4);
+PrintArray(array);
+Console.WriteLine();
+
+
+void SearchSum(int[,] array)
+{
+    int[] sum = new int[array.GetLength(0)];
+
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            sum[j] += array[j, i];
+        }
+        Console.WriteLine($"Сумма {j + 1} строки: {sum[j]}");
+
+    }
+    int min = sum[0];
+    int result = 0;
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+        if (sum[j] < min)
+        {
+            min = sum[j];
+            result = j;
+        }
+    }
+    Console.WriteLine($"Минимальная строка {result + 1} c суммой {min}");
+}
+
+SearchSum(array);
+
---
+++ Example3/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+
+int[,] InitArray(int i, int j)
+{
+    int[,] array = new int[i, j];
+    Random rnd = new Random();
+    for (i = 0; i < array.GetLength(0); i++)
+    {
+        for (j = 0; j < array.GetLength(1); j++)
+        {
+
+            array[i, j] = rnd.Next(0, 10);
+
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine("   ");
+    }
+}
+
+
+int[,] array1 = InitArray(2, 2);
+int[,] array2 = InitArray(2, 2);
+
+PrintArray(array1);
+Console.WriteLine();
+PrintArray(array2);
+
+
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secondMartrix, int[,] resultMatrix)
+{
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < firstMartrix.GetLength(1); k++)
+            {
+                sum += firstMartrix[i, k] * secondMartrix[k, j];
+            }
+            resultMatrix[i, j] = sum;
+        }
+    }
+}
+
+int[,] resultMatrix = InitArray(2, 2);
+
+MultiplyMatrix(array1, array2, resultMatrix);
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+PrintArray(resultMatrix);
